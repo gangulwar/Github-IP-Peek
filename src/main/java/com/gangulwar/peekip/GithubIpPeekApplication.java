1d0e6f0b945c180e19d76cf5ab4403921e09277c
Aarsh Gangulwar <aarshgangulwar23@gmail.com>
2024-05-13 15:51:25 +0530

    add ip endpoint

--- src/main/java/com/gangulwar/peekip/GithubIpPeekApplication.java
+++ src/main/java/com/gangulwar/peekip/GithubIpPeekApplication.java
@@ -1,7 +1,9 @@
 package com.gangulwar.peekip;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
@@ -12,4 +14,10 @@ public class GithubIpPeekApplication {
         SpringApplication.run(GithubIpPeekApplication.class, args);
     }
 
+
+    @GetMapping("/ip")
+    public String getClientIP(HttpServletRequest request) {
+        String ipAddress = request.getRemoteAddr();
+        return "Client IP Address: " + ipAddress;
+    }
 }
